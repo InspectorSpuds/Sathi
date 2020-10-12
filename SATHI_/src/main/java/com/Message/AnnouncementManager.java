f3d149e9bec712264a6d4a1db57976e3e64ed18a
Ishan Parikh <ishanparikh18@gmail.com>
2020-10-12 13:10:53 -0700

    Modal partial functionality, etc.
    I addded a bunch of classes and commands to abstract the code a bit and add some additional functionality. Modal is partially working, opening and submitting is functional still need to work on catching values from the modal fields to create future messages.

---
+++ SATHI_/src/main/java/com/Message/AnnouncementManager.java
@@ -0,0 +1,8 @@
+package com.Message;
+
+import java.util.HashMap;
+
+public class AnnouncementManager {
+    private HashMap<String, Message> announcements= new HashMap<String, Message>(); //stored as: STRING id, Message message
+
+}
---
+++ SATHI_/src/main/java/com/Scraper/Extractor.java
@@ -0,0 +1,5 @@
+package com.Scraper;
+
+public class Extractor {
+
+}
